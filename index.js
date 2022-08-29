ca225eb94cd511d71ba32cf80018c39b4c2ed728
Seo Choi <seochoi56@gmail.com>
2022-08-29 18:11:03 -0400

    commit tree a063a5d32ad7f7038bfad80b74ba553cb2af93fd

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = array => array.slice(0,2)
+
+const returnLastTwoDrivers = array => array.slice(array.length-2, array.length)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    function createFare(fare) {
+        return multiplier * fare
+    }
+    return createFare;
+}
+
+function fareDoubler(fare) {
+    const fareFinder = createFareMultiplier(2);
+    return fareFinder(fare);
+}
+
+const fareTripler = fare => {
+    const fareFinder = createFareMultiplier(3);
+    return fareFinder(fare);
+}
+
+function selectDifferentDrivers(array, arrayFunc) {
+    if (arrayFunc === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(array)
+    } else {
+        return returnLastTwoDrivers(array);
+    }
+    
+}
